79a1f06888695445d48ab5b6b6762331fce96f47
Kogler <e12217672@student.tuwien.ac.at>
2023-12-04 20:41:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+
+HEIGHT = WIDTH = 500
+CELL_SIZE = 25
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+
+clock = pygame.time.Clock()
+FPS = 5
+
+
+if __name__ == '__main__':
+    pygame.init()
+    SCREEN = pygame.display.set_mode((HEIGHT, WIDTH))
+    pygame.display.set_caption("Conway's game of life")
+
+    clock.tick(FPS)
+    run = True
+    while run:
+        SCREEN.fill(BLACK)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        pygame.display.update()
+
