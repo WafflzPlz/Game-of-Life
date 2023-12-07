103fb9463bb92271718efbb23dc96e21f41ca8b9
WafflzPlz <michael.kogler1@gmx.at>
2023-12-07 15:07:44 +0100

    put relevant code into event manager

--- eventmanager.py
+++ eventmanager.py
@@ -1,3 +1,26 @@
+import pygame
+import sys
+import constants
+
 class EventManager:
-    def __init__(self, SCREEN):
+    def __init__(self, SCREEN, grid):
         self.SCREEN = SCREEN
+        self.map = grid
+
+    def close(self):
+        pygame.quit()
+        sys.exit()
+
+    def update(self):
+        events = pygame.event.get()
+        for event in events:
+
+            if event.type == pygame.KEYDOWN:
+
+                # close game
+                if event.key == pygame.K_ESCAPE:
+                    self.close()
+
+            #close game
+            if event.type == pygame.QUIT:
+                self.close()
--- main.py
+++ main.py
@@ -11,27 +11,20 @@ SCREEN = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("Conway's game of life")
 
 map = Map(SCREEN)
-eventmanager = EventManager(SCREEN)
+eventmanager = EventManager(SCREEN, map.grid)
 
+run = True
 def main():
-    run = True
-    while run:
+
+    while True:
         clock.tick(FPS)
         SCREEN.fill(GREY)
+
+        eventmanager.update()
+
         map.draw()
 
-        for event in pygame.event.get():
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    run = False
-
-            if event.type == pygame.QUIT:
-                run = False
-
         pygame.display.update()
-
-
 
 if __name__ == '__main__':
     main()
