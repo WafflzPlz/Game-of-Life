88a6f1e4c6f5abe5b7a77671df5c548a067c7c73
WafflzPlz <michael.kogler1@gmx.at>
2023-12-07 15:35:11 +0100

    added grid line toggle

--- eventmanager.py
+++ eventmanager.py
@@ -1,21 +1,36 @@
 import pygame
 import sys
-import constants
+from constants import *
 
 class EventManager:
-    def __init__(self, SCREEN, grid):
+    def __init__(self, SCREEN, map):
         self.SCREEN = SCREEN
-        self.map = grid
+        self.map = map
 
     def close(self):
         pygame.quit()
         sys.exit()
 
+    def click(self):
+        x, y = pygame.mouse.get_pos()
+        col = x // CELL_SIZE
+        row = y // CELL_SIZE
+        self.map.swap_value(col, row)
+
+    def toggle_grid_lines(self):
+        self.map.toggle_grid_lines()
+
     def update(self):
         events = pygame.event.get()
         for event in events:
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self.click()
+
             if event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_t:
+                    self.toggle_grid_lines()
 
                 # close game
                 if event.key == pygame.K_ESCAPE:
