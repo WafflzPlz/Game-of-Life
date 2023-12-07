3f4e255beae53145ee4ab983523b7da3981547ee
Kogler <e12217672@student.tuwien.ac.at>
2023-12-07 14:22:55 +0100

    fixed wrongly drawing the grid and updated variables in grid initialization

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import pygame
 from constants import *
 class Map:
     def __init__(self, SCREEN):
-        self.grid = [[1 for x in range(GRID_WIDTH)] for y in range(GRID_HEIGHT)]
+        self.grid = [[1 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
         self.SCREEN = SCREEN
 
     def update(self):
@@ -15,6 +15,6 @@ class Map:
         for row in range(len(self.grid)):
             for col in range(len(self.grid[row])):
                 if self.grid[row][col] == 1:
-                    pygame.draw.rect(self.SCREEN, WHITE, pygame.Rect(row * CELL_SIZE, col * CELL_SIZE, GRID_WIDTH, GRID_HEIGHT))
+                    pygame.draw.rect(self.SCREEN, WHITE, pygame.Rect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE))
                 else:
-                    pygame.draw.rect(self.SCREEN, BLACK, pygame.Rect(row * CELL_SIZE, col * CELL_SIZE, GRID_WIDTH, GRID_HEIGHT))
+                    pygame.draw.rect(self.SCREEN, BLACK, pygame.Rect(row * CELL_SIZE, col * CELL_SIZE, CELL_SIZE, CELL_SIZE))
